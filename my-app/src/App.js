418bc3af4513c37b5128c3d8e4cefec6ce10fdc1
Narendra <70422633+Narendra-13@users.noreply.github.com>
2023-11-19 12:56:03 +0530

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Naren from './naren'; // Importing Naren from naren.js
+
+const App = () => {
+  return (
+    <div>
+      <Naren /> {/* Using the renamed component */}
+    </div>
+  );
+};
+
+export default App;
